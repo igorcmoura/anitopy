c5d657540bc811ac0e774a1b1ccd5358536e0518
Igor Cescon de Moura <igor.moura@corp.globo.com>
2017-09-17 17:22:38 -0300

    Docs: add license

--- anitopy/__init__.py
+++ anitopy/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+Anitopy
+~~~~~~~
+
+Anitopy is a python library for parsing anime video filenames.
+
+:copyright: (c) 2017 by Igor Cescon de Moura.
+:license: Mozilla Public License Version 2.0, see LICENSE for more details.
+"""
 
 from anitopy.anitopy import parse
 
