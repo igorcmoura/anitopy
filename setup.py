325f404671b23eb91b54c7e7547853405b5ebc2f
Igor Cescon de Moura <igor.moura@corp.globo.com>
2017-09-29 14:26:33 -0300

    Chore: bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='anitopy',
     packages=['anitopy'],
-    version='0.2.0',
+    version='0.3.0',
     description='An anime video filename parser',
     author='Igor Cescon de Moura',
     author_email='igorcesconm@gmail.com',
