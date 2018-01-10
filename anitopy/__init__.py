42f7d537aacdcc886a1cd203895b2294f605a0ac
Igor Cescon de Moura <igor.moura@corp.globo.com>
2018-01-10 02:44:14 -0200

    Refactor: use absolute import on __init__.py

--- anitopy/__init__.py
+++ anitopy/__init__.py
@@ -8,8 +8,9 @@ Anitopy is a python library for parsing anime video filenames.
 :copyright: (c) 2017 by Igor Cescon de Moura.
 :license: Mozilla Public License Version 2.0, see LICENSE for more details.
 """
+from __future__ import absolute_import
 
-from .anitopy import parse
+from anitopy.anitopy import parse
 
 
 __all__ = ['parse']
