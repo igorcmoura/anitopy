0dc34c68533b1395d2e16dbd0758799254b8f505
Igor Cescon de Moura <igor.moura@corp.globo.com>
2018-01-24 12:08:10 -0200

    Feat: add new audio and video codecs

--- anitopy/__init__.py
+++ anitopy/__init__.py
@@ -5,7 +5,7 @@ Anitopy
 
 Anitopy is a python library for parsing anime video filenames.
 
-:copyright: (c) 2017 by Igor Cescon de Moura.
+:copyright: (c) 2018 by Igor Cescon de Moura.
 :license: Mozilla Public License Version 2.0, see LICENSE for more details.
 """
 from __future__ import absolute_import
--- anitopy/keyword.py
+++ anitopy/keyword.py
@@ -54,8 +54,9 @@ class KeywordManager:
             '2.0CH', '2CH', '5.1', '5.1CH', 'DTS', 'DTS-ES', 'DTS5.1',
             'TRUEHD5.1',
             # Audio codec
-            'AAC', 'AACX2', 'AACX3', 'AACX4', 'AC3', 'FLAC', 'FLACX2',
-            'FLACX3', 'FLACX4', 'LOSSLESS', 'MP3', 'OGG', 'VORBIS',
+            'AAC', 'AACX2', 'AACX3', 'AACX4', 'AC3', 'EAC3', 'E-AC-3',
+            'FLAC', 'FLACX2', 'FLACX3', 'FLACX4', 'LOSSLESS', 'MP3', 'OGG',
+            'VORBIS',
             # Audio language
             'DUALAUDIO', 'DUAL AUDIO'])
 
@@ -113,10 +114,10 @@ class KeywordManager:
             # Frame rate
             '23.976FPS', '24FPS', '29.97FPS', '30FPS', '60FPS', '120FPS',
             # Video codec
-            '8BIT', '8-BIT',
-            '10BIT', '10BITS', '10-BIT', '10-BITS', 'HI10', 'HI10P',
+            '8BIT', '8-BIT', '10BIT', '10BITS', '10-BIT', '10-BITS',
+            'HI10', 'HI10P', 'HI444', 'HI444P', 'HI444PP',
             'H264', 'H265', 'H.264', 'H.265', 'X264', 'X265', 'X.264',
-            'AVC', 'HEVC', 'DIVX', 'DIVX5', 'DIVX6', 'XVID',
+            'AVC', 'HEVC', 'HEVC2', 'DIVX', 'DIVX5', 'DIVX6', 'XVID',
             # Video format
             'AVI', 'RMVB', 'WMV', 'WMV3', 'WMV9',
             # Video quality
