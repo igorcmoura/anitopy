49793c9a95c814ffcdd7d3777c67671985e506a4
Kayabe <lorenzoanonim@yahoo.com>
2022-10-11 18:53:07 +0300

    add test for #13

--- tests/fixtures/table.py
+++ tests/fixtures/table.py
@@ -2613,5 +2613,17 @@ failing_table = [
             'video_resolution': '1080i',
             'volume_number': '03'
         }
+    ], [
+        '[GM-Team][国漫][诛仙][Jade Dynasty][2022][11][HEVC][GB][4K]',
+        None,
+        {
+            'anime_title': 'Jade Dynasty',
+            'anime_year': '2022',
+            'episode_number': '11',
+            'file_name': '[GM-Team][国漫][诛仙][Jade Dynasty][2022][11][HEVC][GB][4K]',
+            'release_group': 'GM-Team',
+            'video_resolution': '4K',
+            'video_term': 'HEVC'
+        }
     ]
 ]
